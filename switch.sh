a6bf76c2fa181ca64b618559c52b780abfd10e0d
sakayoshi <rassy.chrono13@gmail.com>
2023-12-04 12:31:40 +0900

    Create switch.sh
    1. ""内にスピーカー名を入力する
    2. ターミナルで$ chmod a+x ./ファイル名で権限付与する
    3.   ./ファイル名で実行する

---
+++ switch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SwitchAudioSource -t input -s "PHL 246E7" input audio device set to "PHL 246E7"
+SwitchAudioSource -t output -s "PHL 246E7"
+SwitchAudioSource -t system -s "PHL 246E7"
+
+
